632cc27ec71b09cb3adec7c9350426e4576cee3d
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2024-05-17 17:19:18 -0300

    🛠️ - Creating project

---
+++ Handy/Handy Watch App/HandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyApp.swift
+//  Handy Watch App
+//
+//  Created by Leonardo Mesquita Alves on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Handy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
