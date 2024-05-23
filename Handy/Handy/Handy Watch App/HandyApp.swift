f072a0d84b042148171771682f703061ae295ca9
Luca Lacerda <lucagabriel520@gmail.com>
2024-05-23 17:21:41 -0300

    New project added

---
+++ Handy/Handy/Handy Watch App/HandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyApp.swift
+//  Handy Watch App
+//
+//  Created by Luca Lacerda on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Handy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
