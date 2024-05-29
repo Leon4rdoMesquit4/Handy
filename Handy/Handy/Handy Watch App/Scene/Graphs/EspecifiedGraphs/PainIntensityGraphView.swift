03c8623e2439a3e914170a36683242494821a74c
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-05-29 10:35:36 -0300

    feat/pain-intensity-level-graph
    Uma versão rudimentar

--- Handy/Handy/Handy Watch App/Scene/Graphs/EspecifiedGraphs/PainIntensityGraphView.swift
+++ Handy/Handy/Handy Watch App/Scene/Graphs/EspecifiedGraphs/PainIntensityGraphView.swift
@@ -13,9 +13,34 @@ struct PainIntensityGraphView: View {
     @Environment(SwiftDataController.self) var controller
     @Environment(\.modelContext) var context
     @State var graphCase: Coordinator.Destination.GraphCases
+    @State var elements : [Int] = [0, 0, 0, 0, 0, 0]
+    @State var plottedElements : [PlottedElement] = []
     
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            BarChart<Int>(plottedElements: $plottedElements)
+        }
+        .onSubmit {
+            retrieveData()
+        }
+    }
+    
+    private func retrieveData () {
+        let lastWeekDays = controller.getLastWeekDaysForPredicate()
+        let exercises = controller.fetchExercises(context, in: lastWeekDays.0 ... lastWeekDays.1)
+        
+        for exercise in exercises {
+            if let feedback = exercise.painLevel {
+                //plottedElements.append(PlottedElement(image: "intensity\(feedback)", value: 1))
+                elements[Int(feedback)] += 1
+            }
+        }
+        
+        var contador : Int = 0
+        for element in elements {
+            plottedElements.append(PlottedElement(image: "intendity\(contador)", value: element))
+            contador += 1
+        }
     }
     
 }
