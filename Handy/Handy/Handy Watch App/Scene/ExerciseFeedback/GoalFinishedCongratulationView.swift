2fba732e8b2496920c0f78118a9c2eb0e989b975
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2024-08-04 13:04:42 -0300

    feat: Mensagem de orientação para quando não tiver dados nos gráficos

---
+++ Handy/Handy/Handy Watch App/Scene/ExerciseFeedback/GoalFinishedCongratulationView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalFinishedCongratulationView.swift
+//  Handy Watch App
+//
+//  Created by Leonardo Mesquita Alves on 04/08/24.
+//
+
+import SwiftUI
+
+struct GoalFinishedCongratulationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalFinishedCongratulationView()
+}
